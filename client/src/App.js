6ba0f47c49f61f0320e79f27f27e9d9492285581
Maria Nidchenko <marianidchenko@gmail.com>
2022-07-08 21:21:56 +0300

    Added components down to search

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Footer } from "./components/common/Footer";
+import { Header } from "./components/common/Header";
+import { Search } from './components/Search/Search';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <main className="main">
+        <section className="card users-container">
+          <Search />
+        </section>
+      </main>
+
+      <Footer />
     </div>
   );
 }
