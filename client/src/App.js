3648a267b1a9e5284764c0f67508f620928fca1c
Maria Nidchenko <marianidchenko@gmail.com>
2022-07-09 00:02:52 +0300

    added separate user row compontent and set up data

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 import { Footer } from "./components/common/Footer";
 import { Header } from "./components/common/Header";
 import { Search } from './components/Search/Search';
+import { UserSection } from './components/UserSection/UserSection';
 
+const baseUrl = 'http://localhost:3005/api';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${baseUrl}/users`)
+    .then(res => res.json())
+    .then(data => {
+      setUsers(data.users);
+    });
+  }, []);
+
   return (
     <div className="App">
       <Header />
 
       <main className="main">
         <section className="card users-container">
+
           <Search />
+          <UserSection users={users} />
+
         </section>
       </main>
 
